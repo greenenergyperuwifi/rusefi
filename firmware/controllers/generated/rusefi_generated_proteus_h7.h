d6f48e8f31fe4e09164359df4dc244c28420df78
GitHub gen-configs Action <action@github.com>
2025-09-11 23:14:17 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/rusefi_generated_proteus_h7.h
+++ firmware/controllers/generated/rusefi_generated_proteus_h7.h
@@ -2069,7 +2069,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI master.2025.09.11.proteus_h7.685376400"
+#define TS_SIGNATURE "rusEFI master.2025.09.12.proteus_h7.685376400"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
